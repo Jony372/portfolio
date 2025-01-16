632ee8c4688093717ed4c5a6c1d1f51b4e5dda56
Jonathan Tavares Ascencio <jony16344@gmail.com>
2025-01-16 10:13:13 -0600

    About me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   ngOnInit(): void {
     initFlowbite();
+    if(localStorage.getItem('color-theme') === 'dark') {
+      document.documentElement.classList.add('dark');
+    }
+  }
+
+  changeTheme() {
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+      } else {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+      }
+    // if NOT set via local storage previously
+    } else {
+      if (document.documentElement.classList.contains('dark')) {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+      } else {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+      }
+    }
   }
 }
