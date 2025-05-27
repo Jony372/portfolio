6af5accee49fb63832340a58d608d63def6537df
Jonathan Tavares Ascencio <jony16344@gmail.com>
2025-05-27 08:30:14 -0600

    Nuevo proyecto y traduccion del ingles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, HostListener } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,8 @@ import { ProjectsComponent } from './projects/projects.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  language: string = 'es';
+
   ngOnInit(): void {
     initFlowbite();
     if(localStorage.getItem('color-theme') === 'dark') {
@@ -23,6 +26,17 @@ export class AppComponent {
     if(event.key === 't') {
       this.changeTheme();
     }
+  }
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs([this.language]);
+    this.translate.setDefaultLang(this.language);
+    this.translate.use(this.language);
+  }
+
+  changeLanguage() {
+    this.language = this.language === 'es' ? 'en' : 'es';
+    this.translate.use(this.language);
   }
 
   changeTheme() {
